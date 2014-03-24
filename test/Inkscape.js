4a792af9de87e05e1fc62c056f0cdff2b0f8f74a
Andreas Lind Petersen <andreas@one.com>
2014-03-25 00:00:49 +0100

    Replace expect.js with unexpected and update mocha.

--- test/Inkscape.js
+++ test/Inkscape.js
@@ -1,43 +1,43 @@
-var expect = require('expect.js'),
+var expect = require('unexpected'),
     Inkscape = require('../lib/Inkscape'),
     Path = require('path'),
     fs = require('fs');
 
 describe('Inkscape', function () {
     it('should detect the output format as png if -e or --export-png is specified', function () {
-        expect(new Inkscape(['-e']).outputFormat).to.equal('png');
-        expect(new Inkscape(['--export-png']).outputFormat).to.equal('png');
+        expect(new Inkscape(['-e']).outputFormat, 'to equal', 'png');
+        expect(new Inkscape(['--export-png']).outputFormat, 'to equal', 'png');
     });
 
     it('should detect the output format as pdf if -A or --export-pdf is specified', function () {
-        expect(new Inkscape(['-A']).outputFormat).to.equal('pdf');
-        expect(new Inkscape(['--export-pdf']).outputFormat).to.equal('pdf');
+        expect(new Inkscape(['-A']).outputFormat, 'to equal', 'pdf');
+        expect(new Inkscape(['--export-pdf']).outputFormat, 'to equal', 'pdf');
     });
 
     it('should detect the output format as eps if -E or --export-eps is specified', function () {
-        expect(new Inkscape(['-E']).outputFormat).to.equal('eps');
-        expect(new Inkscape(['--export-eps']).outputFormat).to.equal('eps');
+        expect(new Inkscape(['-E']).outputFormat, 'to equal', 'eps');
+        expect(new Inkscape(['--export-eps']).outputFormat, 'to equal', 'eps');
     });
 
     it('should detect the output format as ps if -P or --export-ps is specified', function () {
-        expect(new Inkscape(['-P']).outputFormat).to.equal('ps');
-        expect(new Inkscape(['--export-ps']).outputFormat).to.equal('ps');
+        expect(new Inkscape(['-P']).outputFormat, 'to equal', 'ps');
+        expect(new Inkscape(['--export-ps']).outputFormat, 'to equal', 'ps');
     });
 
     it('should detect the output format as svg if -l or --export-plain-svg is specified', function () {
-        expect(new Inkscape(['-l']).outputFormat).to.equal('svg');
-        expect(new Inkscape(['--export-plain-svg']).outputFormat).to.equal('svg');
+        expect(new Inkscape(['-l']).outputFormat, 'to equal', 'svg');
+        expect(new Inkscape(['--export-plain-svg']).outputFormat, 'to equal', 'svg');
     });
 
     it('the --export-plain-svg=<outputFileName> argument should be injected correctly when -l is specified', function () {
-        expect(new Inkscape(['-l']).inkscapeArgs.some(function (inkscapeArg) {return /^--export-plain-svg=.*\.svg$/.test(inkscapeArg);})).to.be.ok();
+        expect(new Inkscape(['-l']).inkscapeArgs.some(function (inkscapeArg) {return /^--export-plain-svg=.*\.svg$/.test(inkscapeArg);}), 'to be truthy');
     });
 
     it('should produce a PNG when run without arguments', function (done) {
         var inkscape = new Inkscape(),
             chunks = [];
 
-        expect(inkscape.outputFormat).to.equal('png');
+        expect(inkscape.outputFormat, 'to equal', 'png');
         fs.createReadStream(Path.resolve(__dirname, 'test.svg'))
             .pipe(inkscape)
             .on('data', function (chunk) {
@@ -45,11 +45,11 @@ describe('Inkscape', function () {
             })
             .on('end', function () {
                 var resultPngBuffer = Buffer.concat(chunks);
-                expect(resultPngBuffer.length).to.be.greaterThan(4);
-                expect(resultPngBuffer[0]).to.equal(0x89);
-                expect(resultPngBuffer[1]).to.equal(0x50); // P
-                expect(resultPngBuffer[2]).to.equal(0x4e); // N
-                expect(resultPngBuffer[3]).to.equal(0x47); // G
+                expect(resultPngBuffer.length, 'to be greater than', 4);
+                expect(resultPngBuffer[0], 'to equal', 0x89);
+                expect(resultPngBuffer[1], 'to equal', 0x50); // P
+                expect(resultPngBuffer[2], 'to equal', 0x4e); // N
+                expect(resultPngBuffer[3], 'to equal', 0x47); // G
                 done();
             })
             .on('error', done);
@@ -76,7 +76,7 @@ describe('Inkscape', function () {
                 })
                 .on('end', function () {
                     var resultPngBuffer = Buffer.concat(chunks);
-                    expect(resultPngBuffer.length).to.be.greaterThan(0);
+                    expect(resultPngBuffer.length, 'to be greater than', 0);
                     done();
                 });
 
