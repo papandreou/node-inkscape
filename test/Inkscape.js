9a75f0029af15fbeba67f9b4b67fd40e85b21031
Andreas Lind <andreas@one.com>
2016-03-31 16:24:36 +0200

    Implement destroy method, harden the code against leaking resources when errors occur.

--- test/Inkscape.js
+++ test/Inkscape.js
@@ -1,8 +1,11 @@
 /*global describe, it, __dirname, setTimeout*/
-var expect = require('unexpected'),
-    Inkscape = require('../lib/Inkscape'),
-    Path = require('path'),
-    fs = require('fs');
+var expect = require('unexpected')
+    .use(require('unexpected-stream'))
+    .use(require('unexpected-sinon'));
+var sinon = require('sinon');
+var Inkscape = require('../lib/Inkscape');
+var pathModule = require('path');
+var fs = require('fs');
 
 describe('Inkscape', function () {
     it('should detect the output format as png if -e or --export-png is specified', function () {
@@ -34,55 +37,39 @@ describe('Inkscape', function () {
         expect(new Inkscape(['-l']).inkscapeArgs.some(function (inkscapeArg) {return /^--export-plain-svg=.*\.svg$/.test(inkscapeArg);}), 'to be truthy');
     });
 
-    it('should produce a PNG when run without arguments', function (done) {
-        var inkscape = new Inkscape(),
-            chunks = [];
+    it('should produce a PNG when run without arguments', function () {
+        var inkscape = new Inkscape();
 
         expect(inkscape.outputFormat, 'to equal', 'png');
-        fs.createReadStream(Path.resolve(__dirname, 'test.svg'))
-            .pipe(inkscape)
-            .on('data', function (chunk) {
-                chunks.push(chunk);
-            })
-            .on('end', function () {
-                var resultPngBuffer = Buffer.concat(chunks);
-                expect(resultPngBuffer.length, 'to be greater than', 4);
-                expect(resultPngBuffer[0], 'to equal', 0x89);
-                expect(resultPngBuffer[1], 'to equal', 0x50); // P
-                expect(resultPngBuffer[2], 'to equal', 0x4e); // N
-                expect(resultPngBuffer[3], 'to equal', 0x47); // G
-                done();
-            })
-            .on('error', done);
+        return expect(
+            fs.createReadStream(pathModule.resolve(__dirname, 'test.svg')).pipe(inkscape),
+            'to yield output satisfying when decoded as', 'binary', 'to match', /^\x89PNG/
+        );
     });
 
-    it('should not emit data events while paused', function (done) {
+    it('should not emit data events while paused', function () {
         var inkscape = new Inkscape();
 
         function fail() {
-            done(new Error('Inkscape emitted data while it was paused!'));
+            throw new Error('Inkscape emitted data while it was paused!');
         }
         inkscape.pause();
-        inkscape.on('data', fail).on('error', done);
+        inkscape.on('data', fail).on('error', function () {
+        });
 
-        fs.createReadStream(Path.resolve(__dirname, 'test.svg')).pipe(inkscape);
+        expect(inkscape.outputFormat, 'to equal', 'png');
+        fs.createReadStream(pathModule.resolve(__dirname, 'test.svg')).pipe(inkscape);
 
-        setTimeout(function () {
-            inkscape.removeListener('data', fail);
-            var chunks = [];
+        return expect.promise(function (run) {
+            setTimeout(run(function () {
+                inkscape.removeListener('data', fail);
 
-            inkscape
-                .on('data', function (chunk) {
-                    chunks.push(chunk);
-                })
-                .on('end', function () {
-                    var resultPngBuffer = Buffer.concat(chunks);
-                    expect(resultPngBuffer.length, 'to be greater than', 0);
-                    done();
+                inkscape.resume();
+                return expect(inkscape, 'to yield output satisfying', {
+                    length: expect.it('to be greater than', 0)
                 });
-
-            inkscape.resume();
-        }, 1000);
+            }), 1000);
+        });
     });
 
     it('should emit an error if an invalid image is processed', function (done) {
@@ -118,5 +105,113 @@ describe('Inkscape', function () {
         });
 
         inkscape.end(new Buffer('qwvopeqwovkqvwiejvq', 'utf-8'));
+    });
+
+    describe('#destroy', function () {
+        describe('when called before the fs.WriteStream is created', function () {
+            it('should not create the fs.WriteStream or launch the inkscape process', function () {
+                var inkscape = new Inkscape();
+                fs.createReadStream(pathModule.resolve(__dirname, 'test.svg')).pipe(inkscape);
+                inkscape.destroy();
+                return expect.promise(function (run) {
+                    setTimeout(run(function () {
+                        expect(inkscape, 'to satisfy', {
+                            writeStream: expect.it('to be falsy'),
+                            inkscapeProcess: expect.it('to be falsy')
+                        });
+                    }), 10);
+                });
+            });
+        });
+
+        describe('when called while the fs.WriteStream is active', function () {
+            it('should abort the fs.WriteStream and remove the temporary file', function () {
+                var inkscape = new Inkscape();
+                fs.createReadStream(pathModule.resolve(__dirname, 'test.svg')).pipe(inkscape);
+
+                return expect.promise(function (run) {
+                    setTimeout(run(function waitForWriteStream() {
+                        var writeStream = inkscape.writeStream;
+                        if (inkscape.writeStream) {
+                            inkscape.destroy();
+                            expect(inkscape.writeStream, 'to be falsy');
+                            sinon.spy(writeStream, 'end');
+                            sinon.spy(writeStream, 'write');
+                            setTimeout(run(function () {
+                                expect([writeStream.end, writeStream.write], 'to have calls satisfying', []);
+                            }), 10);
+                        } else {
+                            setTimeout(run(waitForWriteStream), 0);
+                        }
+                    }), 0);
+                });
+            });
+        });
+
+        describe('when called while the inkscape process is running', function () {
+            it('should kill the inkscape process and remove the temporary file', function () {
+                var inkscape = new Inkscape();
+                fs.createReadStream(pathModule.resolve(__dirname, 'test.svg')).pipe(inkscape);
+
+                sinon.spy(fs, 'unlink');
+                return expect.promise(function (run) {
+                    setTimeout(run(function waitForInkscapeProcess() {
+                        var inkscapeProcess = inkscape.inkscapeProcess;
+                        if (inkscape.inkscapeProcess) {
+                            sinon.spy(inkscapeProcess, 'kill');
+                            expect(inkscape.filesToCleanUp, 'to satisfy', [
+                                expect.it('to be a string'),
+                                expect.it('to be a string')
+                            ]);
+                            var filesToCleanUp = [].concat(inkscape.filesToCleanUp);
+                            inkscape.destroy();
+                            expect([inkscapeProcess.kill, fs.unlink], 'to have calls satisfying', function () {
+                                inkscapeProcess.kill();
+                                fs.unlink(filesToCleanUp[0], expect.it('to be a function'));
+                                fs.unlink(filesToCleanUp[1], expect.it('to be a function'));
+                            });
+                            expect(inkscape.inkscapeProcess, 'to be falsy');
+                        } else {
+                            setTimeout(run(waitForInkscapeProcess), 0);
+                        }
+                    }), 0);
+                }).finally(function () {
+                    fs.unlink.restore();
+                });
+            });
+        });
+
+        describe('when called while streaming from the temporary output file', function () {
+            it('should kill the inkscape process and remove the temporary output file', function () {
+                var inkscape = new Inkscape();
+                fs.createReadStream(pathModule.resolve(__dirname, 'test.svg')).pipe(inkscape);
+                inkscape.pause();
+                sinon.spy(fs, 'unlink');
+                return expect.promise(function (run) {
+                    setTimeout(run(function waitForReadStream() {
+                        var readStream = inkscape.readStream;
+                        if (readStream) {
+                            sinon.spy(readStream, 'destroy');
+                            expect(inkscape.inkscapeProcess, 'to be falsy');
+                            expect(inkscape.filesToCleanUp, 'to satisfy', [
+                                expect.it('to be a string'),
+                                expect.it('to be a string')
+                            ]);
+                            var filesToCleanUp = [].concat(inkscape.filesToCleanUp);
+                            inkscape.destroy();
+                            expect([fs.unlink, readStream.destroy], 'to have calls satisfying', function () {
+                                readStream.destroy();
+                                fs.unlink(filesToCleanUp[0], expect.it('to be a function'));
+                                fs.unlink(filesToCleanUp[1], expect.it('to be a function'));
+                            });
+                        } else {
+                            setTimeout(run(waitForReadStream), 0);
+                        }
+                    }), 0);
+                }).finally(function () {
+                    fs.unlink.restore();
+                });
+            });
+        });
     });
 });
