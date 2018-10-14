571e4036ff166350a4f03d410820df352e35464f
Andreas Lind <andreaslindpetersen@gmail.com>
2018-10-14 12:41:16 +0200

    for transform in arrow arrow-return let obj-shorthand obj-method ; do for dir in lib test; do lebab --transform $transform --replace $dir; done ; done

--- test/Inkscape.js
+++ test/Inkscape.js
@@ -1,34 +1,34 @@
 /*global describe, it, __dirname, setTimeout*/
-var expect = require('unexpected')
+const expect = require('unexpected')
   .use(require('unexpected-stream'))
   .use(require('unexpected-sinon'));
-var sinon = require('sinon');
-var Inkscape = require('../lib/Inkscape');
-var pathModule = require('path');
-var fs = require('fs');
+const sinon = require('sinon');
+const Inkscape = require('../lib/Inkscape');
+const pathModule = require('path');
+const fs = require('fs');
 
-describe('Inkscape', function() {
-  it('should detect the output format as png if -e or --export-png is specified', function() {
+describe('Inkscape', () => {
+  it('should detect the output format as png if -e or --export-png is specified', () => {
     expect(new Inkscape(['-e']).outputFormat, 'to equal', 'png');
     expect(new Inkscape(['--export-png']).outputFormat, 'to equal', 'png');
   });
 
-  it('should detect the output format as pdf if -A or --export-pdf is specified', function() {
+  it('should detect the output format as pdf if -A or --export-pdf is specified', () => {
     expect(new Inkscape(['-A']).outputFormat, 'to equal', 'pdf');
     expect(new Inkscape(['--export-pdf']).outputFormat, 'to equal', 'pdf');
   });
 
-  it('should detect the output format as eps if -E or --export-eps is specified', function() {
+  it('should detect the output format as eps if -E or --export-eps is specified', () => {
     expect(new Inkscape(['-E']).outputFormat, 'to equal', 'eps');
     expect(new Inkscape(['--export-eps']).outputFormat, 'to equal', 'eps');
   });
 
-  it('should detect the output format as ps if -P or --export-ps is specified', function() {
+  it('should detect the output format as ps if -P or --export-ps is specified', () => {
     expect(new Inkscape(['-P']).outputFormat, 'to equal', 'ps');
     expect(new Inkscape(['--export-ps']).outputFormat, 'to equal', 'ps');
   });
 
-  it('should detect the output format as svg if -l or --export-plain-svg is specified', function() {
+  it('should detect the output format as svg if -l or --export-plain-svg is specified', () => {
     expect(new Inkscape(['-l']).outputFormat, 'to equal', 'svg');
     expect(
       new Inkscape(['--export-plain-svg']).outputFormat,
@@ -37,17 +37,15 @@ describe('Inkscape', function() {
     );
   });
 
-  it('the --export-plain-svg=<outputFileName> argument should be injected correctly when -l is specified', function() {
+  it('the --export-plain-svg=<outputFileName> argument should be injected correctly when -l is specified', () => {
     expect(
-      new Inkscape(['-l']).inkscapeArgs.some(function(inkscapeArg) {
-        return /^--export-plain-svg=.*\.svg$/.test(inkscapeArg);
-      }),
+      new Inkscape(['-l']).inkscapeArgs.some(inkscapeArg => /^--export-plain-svg=.*\.svg$/.test(inkscapeArg)),
       'to be truthy'
     );
   });
 
-  it('should produce a PNG when run without arguments', function() {
-    var inkscape = new Inkscape();
+  it('should produce a PNG when run without arguments', () => {
+    const inkscape = new Inkscape();
 
     expect(inkscape.outputFormat, 'to equal', 'png');
     return expect(
@@ -61,23 +59,23 @@ describe('Inkscape', function() {
     );
   });
 
-  it('should not emit data events while paused', function() {
-    var inkscape = new Inkscape();
+  it('should not emit data events while paused', () => {
+    const inkscape = new Inkscape();
 
     function fail() {
       throw new Error('Inkscape emitted data while it was paused!');
     }
     inkscape.pause();
-    inkscape.on('data', fail).on('error', function() {});
+    inkscape.on('data', fail).on('error', () => {});
 
     expect(inkscape.outputFormat, 'to equal', 'png');
     fs.createReadStream(pathModule.resolve(__dirname, 'test.svg')).pipe(
       inkscape
     );
 
-    return expect.promise(function(run) {
+    return expect.promise(run => {
       setTimeout(
-        run(function() {
+        run(() => {
           inkscape.removeListener('data', fail);
 
           inkscape.resume();
@@ -90,30 +88,30 @@ describe('Inkscape', function() {
     });
   });
 
-  it('should emit an error if an invalid image is processed', function(done) {
-    var inkscape = new Inkscape();
+  it('should emit an error if an invalid image is processed', done => {
+    const inkscape = new Inkscape();
 
     inkscape
-      .on('error', function(err) {
+      .on('error', err => {
         done();
       })
-      .on('data', function(chunk) {
+      .on('data', chunk => {
         done(new Error('Inkscape emitted data when an error was expected'));
       })
-      .on('end', function(chunk) {
+      .on('end', chunk => {
         done(new Error('Inkscape emitted end when an error was expected'));
       });
 
     inkscape.end(new Buffer('qwvopeqwovkqvwiejvq', 'utf-8'));
   });
 
-  it('should emit a single error if an invalid command line is specified', function(done) {
-    var inkscape = new Inkscape(['-vqve']);
+  it('should emit a single error if an invalid command line is specified', done => {
+    const inkscape = new Inkscape(['-vqve']);
 
-    var seenError = false;
+    let seenError = false;
 
     inkscape
-      .on('error', function(err) {
+      .on('error', err => {
         expect(
           inkscape.commandLine,
           'to match',
@@ -126,27 +124,27 @@ describe('Inkscape', function() {
           setTimeout(done, 100);
         }
       })
-      .on('data', function(chunk) {
+      .on('data', chunk => {
         done(new Error('inkscape emitted data when an error was expected'));
       })
-      .on('end', function(chunk) {
+      .on('end', chunk => {
         done(new Error('inkscape emitted end when an error was expected'));
       });
 
     inkscape.end(new Buffer('qwvopeqwovkqvwiejvq', 'utf-8'));
   });
 
-  describe('#destroy', function() {
-    describe('when called before the fs.WriteStream is created', function() {
-      it('should not create the fs.WriteStream or launch the inkscape process', function() {
-        var inkscape = new Inkscape();
+  describe('#destroy', () => {
+    describe('when called before the fs.WriteStream is created', () => {
+      it('should not create the fs.WriteStream or launch the inkscape process', () => {
+        const inkscape = new Inkscape();
         fs.createReadStream(pathModule.resolve(__dirname, 'test.svg')).pipe(
           inkscape
         );
         inkscape.destroy();
-        return expect.promise(function(run) {
+        return expect.promise(run => {
           setTimeout(
-            run(function() {
+            run(() => {
               expect(inkscape, 'to satisfy', {
                 writeStream: expect.it('to be falsy'),
                 inkscapeProcess: expect.it('to be falsy')
@@ -158,24 +156,24 @@ describe('Inkscape', function() {
       });
     });
 
-    describe('when called while the fs.WriteStream is active', function() {
-      it('should abort the fs.WriteStream and remove the temporary file', function() {
-        var inkscape = new Inkscape();
+    describe('when called while the fs.WriteStream is active', () => {
+      it('should abort the fs.WriteStream and remove the temporary file', () => {
+        const inkscape = new Inkscape();
         fs.createReadStream(pathModule.resolve(__dirname, 'test.svg')).pipe(
           inkscape
         );
 
-        return expect.promise(function(run) {
+        return expect.promise(run => {
           setTimeout(
             run(function waitForWriteStream() {
-              var writeStream = inkscape.writeStream;
+              const writeStream = inkscape.writeStream;
               if (inkscape.writeStream) {
                 inkscape.destroy();
                 expect(inkscape.writeStream, 'to be falsy');
                 sinon.spy(writeStream, 'end');
                 sinon.spy(writeStream, 'write');
                 setTimeout(
-                  run(function() {
+                  run(() => {
                     expect(
                       [writeStream.end, writeStream.write],
                       'to have calls satisfying',
@@ -194,31 +192,31 @@ describe('Inkscape', function() {
       });
     });
 
-    describe('when called while the inkscape process is running', function() {
-      it('should kill the inkscape process and remove the temporary file', function() {
-        var inkscape = new Inkscape();
+    describe('when called while the inkscape process is running', () => {
+      it('should kill the inkscape process and remove the temporary file', () => {
+        const inkscape = new Inkscape();
         fs.createReadStream(pathModule.resolve(__dirname, 'test.svg')).pipe(
           inkscape
         );
 
         sinon.spy(fs, 'unlink');
         return expect
-          .promise(function(run) {
+          .promise(run => {
             setTimeout(
               run(function waitForInkscapeProcess() {
-                var inkscapeProcess = inkscape.inkscapeProcess;
+                const inkscapeProcess = inkscape.inkscapeProcess;
                 if (inkscape.inkscapeProcess) {
                   sinon.spy(inkscapeProcess, 'kill');
                   expect(inkscape.filesToCleanUp, 'to satisfy', [
                     expect.it('to be a string'),
                     expect.it('to be a string')
                   ]);
-                  var filesToCleanUp = [].concat(inkscape.filesToCleanUp);
+                  const filesToCleanUp = [].concat(inkscape.filesToCleanUp);
                   inkscape.destroy();
                   expect(
                     [inkscapeProcess.kill, fs.unlink],
                     'to have calls satisfying',
-                    function() {
+                    () => {
                       inkscapeProcess.kill();
                       fs.unlink(
                         filesToCleanUp[0],
@@ -238,25 +236,25 @@ describe('Inkscape', function() {
               0
             );
           })
-          .finally(function() {
+          .finally(() => {
             fs.unlink.restore();
           });
       });
     });
 
-    describe('when called while streaming from the temporary output file', function() {
-      it('should kill the inkscape process and remove the temporary output file', function() {
-        var inkscape = new Inkscape();
+    describe('when called while streaming from the temporary output file', () => {
+      it('should kill the inkscape process and remove the temporary output file', () => {
+        const inkscape = new Inkscape();
         fs.createReadStream(pathModule.resolve(__dirname, 'test.svg')).pipe(
           inkscape
         );
         inkscape.pause();
         sinon.spy(fs, 'unlink');
         return expect
-          .promise(function(run) {
+          .promise(run => {
             setTimeout(
               run(function waitForReadStream() {
-                var readStream = inkscape.readStream;
+                const readStream = inkscape.readStream;
                 if (readStream) {
                   sinon.spy(readStream, 'destroy');
                   expect(inkscape.inkscapeProcess, 'to be falsy');
@@ -264,12 +262,12 @@ describe('Inkscape', function() {
                     expect.it('to be a string'),
                     expect.it('to be a string')
                   ]);
-                  var filesToCleanUp = [].concat(inkscape.filesToCleanUp);
+                  const filesToCleanUp = [].concat(inkscape.filesToCleanUp);
                   inkscape.destroy();
                   expect(
                     [fs.unlink, readStream.destroy],
                     'to have calls satisfying',
-                    function() {
+                    () => {
                       readStream.destroy();
                       fs.unlink(
                         filesToCleanUp[0],
@@ -288,7 +286,7 @@ describe('Inkscape', function() {
               0
             );
           })
-          .finally(function() {
+          .finally(() => {
             fs.unlink.restore();
           });
       });
@@ -297,9 +295,9 @@ describe('Inkscape', function() {
 
   // Doesn't seem to work on Travis, probably due to no X being installed
   if (!process.env.CI) {
-    describe('when utilizing verbs', function() {
-      it('should operate in GUI mode', function() {
-        var inkscape = new Inkscape([
+    describe('when utilizing verbs', () => {
+      it('should operate in GUI mode', () => {
+        const inkscape = new Inkscape([
           '--verb=EditDeselect',
           '--select=layer9',
           '--verb=SelectionUnion',
@@ -311,8 +309,8 @@ describe('Inkscape', function() {
         expect(inkscape.commandLine, 'not to contain', '--without-gui');
       });
 
-      it('should treat the input file as the output file (assuming --verb=FileSave)', function() {
-        var inkscape = new Inkscape([
+      it('should treat the input file as the output file (assuming --verb=FileSave)', () => {
+        const inkscape = new Inkscape([
           '--verb=EditDeselect',
           '--select=layer9',
           '--verb=SelectionUnion',
